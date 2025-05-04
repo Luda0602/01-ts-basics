64e2cd9826bd162cacdd04d2db312ad932fec417
Ludmila <ludamocuk29@gmail.com>
2025-05-04 13:25:07 +0300

    flished task-8

--- src/task-8.ts
+++ src/task-8.ts
@@ -1,19 +1,18 @@
 import axios from "axios";
 
-// Інтерфейс описує структуру одного поста
 interface Post {
-  id: number;
-  title: string;
-  body: string;
+    id: number;
+    title: string;
+    body: string;
 }
 
-// Функція з правильною типізацією відповіді — масив Post
 async function fetchPosts(): Promise<Post[]> {
-  const response = await axios.get<Post[]>("https://jsonplaceholder.typicode.com/posts");
+  const response = await axios.get<Post[]>(
+    '<https://jsonplaceholder.typicode.com/posts>'
+  );
   return response.data;
 }
 
-// Виклик функції
 fetchPosts().then((posts) => {
   console.log(posts[0].title);
 });
